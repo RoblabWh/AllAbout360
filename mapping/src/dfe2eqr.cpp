e8cddcaaa653fb7d0f9541561f84895223d9b69c
Niklas Digakis <niklas.digakis@studmail.w-hs.de>
2021-03-10 21:43:27 +0100

    image input support

--- mapping/src/dfe2eqr.cpp
+++ mapping/src/dfe2eqr.cpp
@@ -57,14 +57,24 @@ void read_mapping_file(string path, cv::Mat &mapping_table)
 		{
 			if (file >> mte[0] >> mte[1] >> mte[2] >> mte[3] >> mte[4])
 			{
+				if (mte[0] < 0 || mte[0] >= height || mte[1] < 0 || mte[1] >= height || mte[2] < 0 || mte[2] >= height || mte[3] < 0 || mte[3] >= height || mte[4] < 0 || mte[4] > 1)
+				{
+					cerr << "Error reading mapping file \"" << path << "\" on line " << j * width + i + 2 << ", value out of range." << endl;
+					exit(EXIT_FAILURE);
+				}
 				mapping_table.at<cv::Vec5d>(j, i) = mte;
 			}
 			else
 			{
-				cerr << "Error reading mapping file \"" << path << "\" on line " << j * width + i + 1 << '.' << endl;
+				cerr << "Error reading mapping file \"" << path << "\" on line " << j * width + i + 2 << '.' << endl;
 				exit(EXIT_FAILURE);
 			}
 		}
+	}
+	if (file >> mte[0])
+	{
+		cerr << "Error reading mapping file \"" << path << "\" on line " << height * width + 1 << ", too many entries." << endl;
+		exit(EXIT_FAILURE);
 	}
 }
 
@@ -166,6 +176,98 @@ void remap(const cv::Mat &in_1, const cv::Mat &in_2, const cv::Mat &map, cv::Mat
 	}
 }
 
+template <typename T, typename ACC_FN>
+void loop(T in, ACC_FN fn, const cv::Mat &map, cv::Mat &equiframe)
+{
+	cv::Mat frame;
+	TIMES(
+		unsigned int nrframes = 0; double mapping_sum = 0, loading_sum = 0, preview_sum = 0;
+		std::chrono::steady_clock::time_point mapping_start, mapping_end, loading_start, loading_end, preview_start, preview_end;)
+	while (cv::waitKey(1) != 27)
+	{
+		TIMES(loading_start = std::chrono::steady_clock::now();)
+		// get a new frame from camera
+		(in.*fn)(frame);
+		if (frame.empty())
+			break;
+		TIMES(loading_end = std::chrono::steady_clock::now();
+			  loading_sum += std::chrono::duration_cast<std::chrono::duration<double>>(loading_end - loading_start).count();)
+
+		TIMES(mapping_start = std::chrono::steady_clock::now();)
+		// remap(frame, map, equiframe);
+		remap(frame, frame(cv::Rect(map.rows, 0, map.rows, map.rows)), map, equiframe);
+		TIMES(mapping_end = std::chrono::steady_clock::now();
+			  mapping_sum += std::chrono::duration_cast<std::chrono::duration<double>>(mapping_end - mapping_start).count();)
+
+		TIMES(preview_start = std::chrono::steady_clock::now();)
+		// Auf den Schirm Spoki
+		imshow(WINDOW_NAME, equiframe);
+		// if (wrt.isOpened())
+		// 	wrt << equiframe;
+		TIMES(preview_end = std::chrono::steady_clock::now();
+			  preview_sum += std::chrono::duration_cast<std::chrono::duration<double>>(preview_end - preview_start).count();)
+
+		// check time
+		TIMES(nrframes++;
+			  if ((nrframes % 300) == 0) {
+				  printf("frame nr: %u\n", nrframes);
+				  printf("%f avg mapping time\n", mapping_sum / nrframes);
+				  printf("%f avg loading time\n", loading_sum / nrframes);
+				  printf("%f avg preview time\n", preview_sum / nrframes);
+				  printf("%f fps mapping\n", nrframes / mapping_sum);
+				  printf("%f fps mapping + loading\n", nrframes / (mapping_sum + loading_sum));
+				  printf("%f fps mapping + loading + preview\n", nrframes / (mapping_sum + loading_sum + preview_sum));
+				  printf("\n");
+			  })
+	}
+}
+
+template <typename T, typename ACC_FN>
+void loop(T in_1, T in_2, ACC_FN fn, const cv::Mat &map, cv::Mat &equiframe)
+{
+	cv::Mat frame_1, frame_2;
+	TIMES(
+		unsigned int nrframes = 0; double mapping_sum = 0, loading_sum = 0, preview_sum = 0;
+		std::chrono::steady_clock::time_point mapping_start, mapping_end, loading_start, loading_end, preview_start, preview_end;)
+	while (cv::waitKey(1) != 27)
+	{
+		TIMES(loading_start = std::chrono::steady_clock::now();)
+		// get a new frame from camera
+		(in_1.*fn)(frame_1);
+		(in_2.*fn)(frame_2);
+		if (frame_1.empty() || frame_2.empty())
+			break;
+		TIMES(loading_end = std::chrono::steady_clock::now();
+			  loading_sum += std::chrono::duration_cast<std::chrono::duration<double>>(loading_end - loading_start).count();)
+
+		TIMES(mapping_start = std::chrono::steady_clock::now();)
+		remap(frame_1, frame_2, map, equiframe);
+		TIMES(mapping_end = std::chrono::steady_clock::now();
+			  mapping_sum += std::chrono::duration_cast<std::chrono::duration<double>>(mapping_end - mapping_start).count();)
+
+		TIMES(preview_start = std::chrono::steady_clock::now();)
+		// Auf den Schirm Spoki
+		imshow(WINDOW_NAME, equiframe);
+		// if (wrt.isOpened())
+		// 	wrt << equiframe;
+		TIMES(preview_end = std::chrono::steady_clock::now();
+			  preview_sum += std::chrono::duration_cast<std::chrono::duration<double>>(preview_end - preview_start).count();)
+
+		// check time
+		TIMES(nrframes++;
+			  if ((nrframes % 300) == 0) {
+				  printf("frame nr: %u\n", nrframes);
+				  printf("%f avg mapping time\n", mapping_sum / nrframes);
+				  printf("%f avg loading time\n", loading_sum / nrframes);
+				  printf("%f avg preview time\n", preview_sum / nrframes);
+				  printf("%f fps mapping\n", nrframes / mapping_sum);
+				  printf("%f fps mapping + loading\n", nrframes / (mapping_sum + loading_sum));
+				  printf("%f fps mapping + loading + preview\n", nrframes / (mapping_sum + loading_sum + preview_sum));
+				  printf("\n");
+			  })
+	}
+}
+
 int main(int argc, char **argv)
 {
 	string map_file_path, vid_path_1, vid_path_2, out_path;
@@ -185,6 +287,9 @@ int main(int argc, char **argv)
 	// create windows
 	cv::namedWindow(WINDOW_NAME, CV_WINDOW_KEEPRATIO | CV_WINDOW_NORMAL);
 	cv::resizeWindow(WINDOW_NAME, 1440, 720);
+
+	// init output mat
+	equiframe.create(mapping_table.rows, mapping_table.cols, CV_8UC3);
 
 	//TODO do better implementation
 	cv::VideoWriter wrt;
@@ -207,49 +312,71 @@ int main(int argc, char **argv)
 			return EXIT_FAILURE;
 		}
 
-		// init output mat
-		equiframe.create(mapping_table.rows, mapping_table.cols, frame_1.type());
+		// check resolution
+		if (frame_1.cols != mapping_table.cols || frame_1.rows != mapping_table.rows)
+		{
+			cout << "Image(" << frame_1.cols << 'x' << frame_1.rows
+				 << ") and map(" << mapping_table.cols << 'x' << mapping_table.rows << ") resolution don't match." << endl;
+			return EXIT_FAILURE;
+		}
 
+		// process single image
+		cap_1 >> frame_2;
+		if (frame_2.empty())
+		{
+			remap(frame_1, mapping_table, equiframe);
+			cv::imshow(WINDOW_NAME, equiframe);
+			if (!out_path.empty())
+				cv::imwrite(out_path, equiframe);
+			cv::waitKey();
+			return EXIT_SUCCESS;
+		}
+
+		// jump back to begining
+		cap_1.set(cv::CAP_PROP_POS_FRAMES, 0);
+
+		// TODO open video output
 		if (!out_path.empty())
 			wrt.open(out_path, cap_1.get(cv::CAP_PROP_FOURCC), cap_1.get(cv::CAP_PROP_FPS), cv::Size(mapping_table.cols, mapping_table.rows));
 
 		// do mapping
-		while (cv::waitKey(1) != 27)
-		{
-			TIMES(mapping_start = std::chrono::steady_clock::now();)
-			remap(frame_1, mapping_table, equiframe);
-			TIMES(mapping_end = std::chrono::steady_clock::now();
-				  mapping_sum += std::chrono::duration_cast<std::chrono::duration<double>>(mapping_end - mapping_start).count();)
+		loop(cap_1, &cv::VideoCapture::read, mapping_table, equiframe);
+		// while (cv::waitKey(1) != 27)
+		// {
+		// 	TIMES(mapping_start = std::chrono::steady_clock::now();)
+		// 	remap(frame_1, mapping_table, equiframe);
+		// 	TIMES(mapping_end = std::chrono::steady_clock::now();
+		// 		  mapping_sum += std::chrono::duration_cast<std::chrono::duration<double>>(mapping_end - mapping_start).count();)
 
-			TIMES(preview_start = std::chrono::steady_clock::now();)
-			// Auf den Schirm Spoki
-			imshow(WINDOW_NAME, equiframe);
-			if (wrt.isOpened())
-				wrt << equiframe;
-			TIMES(preview_end = std::chrono::steady_clock::now();
-				  preview_sum += std::chrono::duration_cast<std::chrono::duration<double>>(preview_end - preview_start).count();)
+		// 	TIMES(preview_start = std::chrono::steady_clock::now();)
+		// 	// Auf den Schirm Spoki
+		// 	imshow(WINDOW_NAME, equiframe);
+		// 	if (wrt.isOpened())
+		// 		wrt << equiframe;
+		// 	TIMES(preview_end = std::chrono::steady_clock::now();
+		// 		  preview_sum += std::chrono::duration_cast<std::chrono::duration<double>>(preview_end - preview_start).count();)
 
-			TIMES(loading_start = std::chrono::steady_clock::now();)
-			// get a new frame from camera
-			cap_1 >> frame_1;
-			if (frame_1.empty())
-				break;
-			TIMES(loading_end = std::chrono::steady_clock::now();
-				  loading_sum += std::chrono::duration_cast<std::chrono::duration<double>>(loading_end - loading_start).count();)
+		// 	TIMES(loading_start = std::chrono::steady_clock::now();)
+		// 	// get a new frame from camera
+		// 	cap_1 >> frame_1;
+		// 	if (frame_1.empty())
+		// 		break;
+		// 	TIMES(loading_end = std::chrono::steady_clock::now();
+		// 		  loading_sum += std::chrono::duration_cast<std::chrono::duration<double>>(loading_end - loading_start).count();)
 
-			// check time
-			TIMES(nrframes++;
-				  if ((nrframes % 300) == 0) {
-					  printf("frame nr: %u\n", nrframes);
-					  printf("%f avg mapping time\n", mapping_sum / nrframes);
-					  printf("%f avg loading time\n", loading_sum / nrframes);
-					  printf("%f avg preview time\n", preview_sum / nrframes);
-					  printf("%f fps mapping\n", nrframes / mapping_sum);
-					  printf("%f fps mapping + loading\n", nrframes / (mapping_sum + loading_sum));
-					  printf("%f fps mapping + loading + preview\n", nrframes / (mapping_sum + loading_sum + preview_sum));
-					  printf("\n");
-				  })
-		}
+		// 	// check time
+		// 	TIMES(nrframes++;
+		// 		  if ((nrframes % 300) == 0) {
+		// 			  printf("frame nr: %u\n", nrframes);
+		// 			  printf("%f avg mapping time\n", mapping_sum / nrframes);
+		// 			  printf("%f avg loading time\n", loading_sum / nrframes);
+		// 			  printf("%f avg preview time\n", preview_sum / nrframes);
+		// 			  printf("%f fps mapping\n", nrframes / mapping_sum);
+		// 			  printf("%f fps mapping + loading\n", nrframes / (mapping_sum + loading_sum));
+		// 			  printf("%f fps mapping + loading + preview\n", nrframes / (mapping_sum + loading_sum + preview_sum));
+		// 			  printf("\n");
+		// 		  })
+		// }
 	}
 	else // two inputs
 	{
@@ -270,52 +397,74 @@ int main(int argc, char **argv)
 			cout << "Video file \"" << vid_path_1 << "\" is empty." << endl;
 		if (frame_2.empty())
 			cout << "Video file \"" << vid_path_2 << "\" is empty." << endl;
-		if (frame_1.empty() || frame_1.empty())
+		if (frame_1.empty() || frame_2.empty())
 			return EXIT_FAILURE;
 
-		// init output mat
-		equiframe.create(mapping_table.rows, mapping_table.cols, frame_1.type());
+		// check resolution
+		if (frame_1.cols * 2 != mapping_table.cols || frame_1.rows != mapping_table.rows || frame_2.cols * 2 != mapping_table.cols || frame_2.rows != mapping_table.rows)
+		{
+			cout << "Image1(" << frame_1.cols << 'x' << frame_1.rows << "), Image2(" << frame_2.cols << 'x' << frame_2.rows
+				 << ") and map(" << mapping_table.cols << 'x' << mapping_table.rows << ") resolution don't match." << endl;
+			return EXIT_FAILURE;
+		}
+
+		// process single image
+		if (!cap_1.grab())
+		{
+			remap(frame_1, frame_2, mapping_table, equiframe);
+			cv::imshow(WINDOW_NAME, equiframe);
+			if (!out_path.empty())
+				cv::imwrite(out_path, equiframe);
+			cv::waitKey();
+			return EXIT_SUCCESS;
+		}
+
+		// jump back to begining
+		cap_1.set(cv::CAP_PROP_POS_FRAMES, 0);
+		cap_2.set(cv::CAP_PROP_POS_FRAMES, 0);
 
 		if (!out_path.empty())
 			wrt.open(out_path, cap_1.get(cv::CAP_PROP_FOURCC), cap_1.get(cv::CAP_PROP_FPS), cv::Size(mapping_table.cols, mapping_table.rows));
 
-		while (cv::waitKey(1) != 27)
-		{
-			TIMES(mapping_start = std::chrono::steady_clock::now();)
-			remap(frame_1, frame_2, mapping_table, equiframe);
-			TIMES(mapping_end = std::chrono::steady_clock::now();
-				  mapping_sum += std::chrono::duration_cast<std::chrono::duration<double>>(mapping_end - mapping_start).count();)
+		// do mapping
+		loop(cap_1, cap_2, &cv::VideoCapture::read, mapping_table, equiframe);
+		// while (cv::waitKey(1) != 27)
+		// {
+		// 	TIMES(mapping_start = std::chrono::steady_clock::now();)
+		// 	remap(frame_1, frame_2, mapping_table, equiframe);
+		// 	TIMES(mapping_end = std::chrono::steady_clock::now();
+		// 		  mapping_sum += std::chrono::duration_cast<std::chrono::duration<double>>(mapping_end - mapping_start).count();)
 
-			TIMES(preview_start = std::chrono::steady_clock::now();)
-			// Auf den Schirm Spoki
-			imshow(WINDOW_NAME, equiframe);
-			if (wrt.isOpened())
-				wrt << equiframe;
-			TIMES(preview_end = std::chrono::steady_clock::now();
-				  preview_sum += std::chrono::duration_cast<std::chrono::duration<double>>(preview_end - preview_start).count();)
+		// 	TIMES(preview_start = std::chrono::steady_clock::now();)
+		// 	// Auf den Schirm Spoki
+		// 	imshow(WINDOW_NAME, equiframe);
+		// 	if (wrt.isOpened())
+		// 		wrt << equiframe;
+		// 	TIMES(preview_end = std::chrono::steady_clock::now();
+		// 		  preview_sum += std::chrono::duration_cast<std::chrono::duration<double>>(preview_end - preview_start).count();)
 
-			TIMES(loading_start = std::chrono::steady_clock::now();)
-			// get a new frame from camera
-			cap_1 >> frame_1;
-			cap_2 >> frame_2;
-			if (frame_1.empty() || frame_2.empty())
-				break;
-			TIMES(loading_end = std::chrono::steady_clock::now();
-				  loading_sum += std::chrono::duration_cast<std::chrono::duration<double>>(loading_end - loading_start).count();)
+		// 	TIMES(loading_start = std::chrono::steady_clock::now();)
+		// 	// get a new frame from camera
+		// 	cap_1 >> frame_1;
+		// 	cap_2 >> frame_2;
+		// 	if (frame_1.empty() || frame_2.empty())
+		// 		break;
+		// 	TIMES(loading_end = std::chrono::steady_clock::now();
+		// 		  loading_sum += std::chrono::duration_cast<std::chrono::duration<double>>(loading_end - loading_start).count();)
 
-			// check time
-			TIMES(nrframes++;
-				  if ((nrframes % 300) == 0) {
-					  printf("frame nr: %u\n", nrframes);
-					  printf("%f avg mapping time\n", mapping_sum / nrframes);
-					  printf("%f avg loading time\n", loading_sum / nrframes);
-					  printf("%f avg preview time\n", preview_sum / nrframes);
-					  printf("%f fps mapping\n", nrframes / mapping_sum);
-					  printf("%f fps mapping + loading\n", nrframes / (mapping_sum + loading_sum));
-					  printf("%f fps mapping + loading + preview\n", nrframes / (mapping_sum + loading_sum + preview_sum));
-					  printf("\n");
-				  })
-		}
+		// 	// check time
+		// 	TIMES(nrframes++;
+		// 		  if ((nrframes % 300) == 0) {
+		// 			  printf("frame nr: %u\n", nrframes);
+		// 			  printf("%f avg mapping time\n", mapping_sum / nrframes);
+		// 			  printf("%f avg loading time\n", loading_sum / nrframes);
+		// 			  printf("%f avg preview time\n", preview_sum / nrframes);
+		// 			  printf("%f fps mapping\n", nrframes / mapping_sum);
+		// 			  printf("%f fps mapping + loading\n", nrframes / (mapping_sum + loading_sum));
+		// 			  printf("%f fps mapping + loading + preview\n", nrframes / (mapping_sum + loading_sum + preview_sum));
+		// 			  printf("\n");
+		// 		  })
+		// }
 	}
 
 	// When everything done, release the video capture object
